67c42bee7b61b619017255f97b1a3cbb977c05e9
Raymond Lochner <ray_lochner@yahoo.de>
2018-02-17 10:13:08 +0100

    Adding gtfs-db select queries for the node app

--- app/db/queries/selects.js
+++ app/db/queries/selects.js
@@ -1,7 +1,23 @@
 const db = require('../index');
 
-function doQuery(queryString, body, callback) {
-  db.query(queryString, body)
+function whereStatementCreation(attributes, values) {
+  if (attributes.length > 0 && attributes.length === values.length) {
+    let whereString = ' WHERE ';
+    for (let i = 0; i < attributes.length; i += 1) {
+      // Adding 'AND' for every where condition that is not the first
+      if (i > 0) {
+        whereString += ' AND ';
+      }
+      whereString += `${attributes[i]} = ${values[i]}`;
+    }
+    return whereString;
+  }
+  return '';
+}
+
+function doQuery(query, attributes, values, callback) {
+  const queryString = query + whereStatementCreation(attributes, values);
+  db.query(queryString)
     .then((dbRes) => {
       callback(null, dbRes);
     })
@@ -9,3 +25,72 @@ function doQuery(queryString, body, callback) {
       callback(dbErr);
     });
 }
+
+const functions = {
+  getAgency(attributes, values, callback) {
+    const queryString = 'SELECT * FROM agency';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getStops(attributes, values, callback) {
+    const queryString = 'SELECT * FROM stops';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getRoutes(attributes, values, callback) {
+    const queryString = 'SELECT * FROM routes';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getCalendar(attributes, values, callback) {
+    const queryString = 'SELECT * FROM calendar';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getCalendarDate(attributes, values, callback) {
+    const queryString = 'SELECT * FROM calendar_dates';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getFareAttributes(attributes, values, callback) {
+    const queryString = 'SELECT * FROM fare_attributes';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getFareFareRules(attributes, values, callback) {
+    const queryString = 'SELECT * FROM fare_rules';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getShapes(attributes, values, callback) {
+    const queryString = 'SELECT * FROM shapes';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getTrips(attributes, values, callback) {
+    const queryString = 'SELECT * FROM trips';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getStopTimes(attributes, values, callback) {
+    const queryString = 'SELECT * FROM stop_times';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getFrequencies(attributes, values, callback) {
+    const queryString = 'SELECT * FROM frequencies';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getTransfers(attributes, values, callback) {
+    const queryString = 'SELECT * FROM transfers';
+    doQuery(queryString, attributes, values, callback);
+  },
+
+  getFeedInfo(attributes, values, callback) {
+    const queryString = 'SELECT * FROM feed_info';
+    doQuery(queryString, attributes, values, callback);
+  },
+};
+
+export default functions;
